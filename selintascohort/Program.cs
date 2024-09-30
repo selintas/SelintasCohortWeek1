c6a061924cba738925c9fef474f53945d185f793
selintas <sselintas@gmail.com>
2024-09-30 03:48:06 -0700

    Add project files.

---
+++ selintascohort/Program.cs
@@ -0,0 +1,12 @@
+﻿//#c - Self-introduction
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Selin Tas");
+
+        Console.WriteLine("I am keen to improve myself in software.");
+    }
+}
